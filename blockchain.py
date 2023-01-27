d7f6375db1cd6c7d4b513313b61adac1a5b2024f
Jai Sawhney <jai.sawhney1@gmail.com>
2023-01-26 16:27:12 -0800

    Add main Blockchain class methods

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,40 @@
+import hashlib
+import json
+from datetime import time
+
+
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.new_block(proof=100, previous_hash=1)
 
-    def new_block(self):
-        pass
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
 
-    def new_transaction(self):
-        pass
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append(
+            {
+                'sender': sender,
+                'recipient': recipient,
+                'amount': amount
+            }
+        )
 
     @staticmethod
     def hash(block):
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
